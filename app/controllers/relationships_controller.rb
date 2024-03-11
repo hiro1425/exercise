f1389d1d5cdfe3290c991d871b2d86ad998c58f3
hiro1425 <goegoe1425@gmail.com>
2024-03-11 16:58:35 +0900

    フォローボタンの実装

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,15 @@
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user!, except: [:create, :destroy]
+
+  def create
+    follow = current_user.active_relationships.new(follower_id: params[:user_id])
+    follow.save
+    redirect_to post_path(params[:user_id])
+  end
+
+  def destroy
+    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    follow.destroy
+    redirect_to post_path(params[:user_id])
+  end
+end
